9c8a6a0c70e51238336d1caf91c18344daadc931
John Hook <jhook1@ufl.edu>
2021-10-18 17:15:56 -0400

    Async message working. TODO: add wrappers for reply/dm

--- cplusplus/soup-client/messaging.cpp
+++ cplusplus/soup-client/messaging.cpp
@@ -11,7 +11,10 @@ int main(){
 	std::string chatSvcAggToken;
 
     readCredentials(skypeToken, chatSvcAggToken);
+    //remove new line from skypeToken
     skypeToken.erase(skypeToken.size()-1,1);
+
+    GMainLoop *loop = g_main_loop_new(NULL,false);
 
     //John/Olga channel
 	std::string channelId = "19:0MaeOcpNpAX-HchAP2Z8xnw6j_QYsq6htWoAsD94QxY1@thread.tacv2";
@@ -19,9 +22,14 @@ int main(){
     SoupSession *session = soup_session_new();
 
     //fetchTeamsSync(session,chatSvcAggToken);
-    std::string msgtext = "Souptest";
-    sendMessageSync(session,msgtext,skypeToken,channelId);
-    //sendChannelMessage(session,msgtext,skypeToken,channelId);
+    std::string msgtext = "Async test";
+    //sendMessageSync(session,msgtext,skypeToken,channelId);
+    sendChannelMessage(session,loop,msgtext,skypeToken,channelId);
+
+    g_main_loop_run(loop);
+
+    g_main_loop_unref(loop);
+    g_object_unref(session);
 
     return 0;
 }
@@ -71,7 +79,7 @@ void fetchTeams(SoupSession *session, std::string &chatSvcAggToken){
     std::string tokenstr = "Bearer " + chatSvcAggToken;
     soup_message_headers_append(msg->request_headers,"Authorization",tokenstr.c_str());
 
-    soup_session_send_async(session,msg,NULL,sendMessageCallback,NULL);
+    //soup_session_send_async(session,msg,NULL,sendMessageCallback,NULL);
 }
 
 void sendMessageSync(SoupSession *session, std::string &text, std::string &skypeToken, std::string &params){
@@ -79,11 +87,11 @@ void sendMessageSync(SoupSession *session, std::string &text, std::string &skype
     std::string url = "https://amer.ng.msg.teams.microsoft.com/v1/users/ME/conversations/" + params + "/messages";
     SoupMessage *msg = soup_message_new(SOUP_METHOD_POST,url.c_str());
 
-    std::string payload = "{'content':'"+text+"','messagetype':'Text','contenttype':'text','asmreferences':[],'properties':{'importance':'','subject':null}}";
-    //char *payload = soup_form_encode("content",text,"messagetype","Text","contenttype","text","asmreferences","[]","properties","{importance:'',subject:null}",NULL);
-    //soup_message_set_request(msg,"application/x-www-form-urlencoded",SOUP_MEMORY_COPY, payload, strlen(payload));
+    //set message request payload
+    std::string payload = "{'content':\""+text+"\",'messagetype':'Text','contenttype':'text','asmreferences':[],'properties':{'importance':'','subject':null}}";
     soup_message_set_request(msg,"application/json",SOUP_MEMORY_COPY,payload.c_str(),strlen(payload.c_str()));
 
+    //set request auth header token
     std::string tokenstr = "skypetoken=" + skypeToken;
     soup_message_headers_append(msg->request_headers,"Authentication",tokenstr.c_str());
 
@@ -110,51 +118,35 @@ void sendMessageSync(SoupSession *session, std::string &text, std::string &skype
     free(resbuff);
 }
 
-void sendMessage(SoupSession *session, std::string &text, std::string &skypeToken, std::string &params){
+void sendMessage(SoupSession *session, GMainLoop *loop, std::string &text, std::string &skypeToken, std::string &params){
     //initialize soup message with url and method
     std::string url = "https://amer.ng.msg.teams.microsoft.com/v1/users/ME/conversations/" + params + "/messages";
     SoupMessage *msg = soup_message_new(SOUP_METHOD_POST,url.c_str());
 
     //set message request payload
-    //std::string payload = "{content:" + text + ",messagetype:Text,cotenttype:text,asmreferences:[],properties:{importance:'',subject:null}}";
-    std::string payload = soup_form_encode("content",text,"messagetype","Text","contenttype","text","asmreferences","[]","properties","{importance:'',subject:null}",NULL);
-    soup_message_set_request(msg,"application/x-www-form-urlencoded",SOUP_MEMORY_COPY, payload.c_str(), strlen(payload.c_str()));
+    std::string payload = "{'content':\""+text+"\",'messagetype':'Text','contenttype':'text','asmreferences':[],'properties':{'importance':'','subject':null}}";
+    soup_message_set_request(msg,"application/json",SOUP_MEMORY_COPY,payload.c_str(),strlen(payload.c_str()));
 
     //set request auth header token
     std::string tokenstr = "skypetoken=" + skypeToken;
     soup_message_headers_append(msg->request_headers,"Authentication",tokenstr.c_str());
 
     //send async message request
-    soup_session_send_async(session,msg,NULL,sendMessageCallback,NULL);
+    soup_session_queue_message(session,msg,queueMessageCallback,loop);
 }
 
-void sendMessageCallback(GObject *obj, GAsyncResult *res, gpointer user_data){
-	std::cout << "Callback" << std::endl;
-    
-    GError *error = NULL;
-    GInputStream *stream = soup_session_send_finish (SOUP_SESSION (obj), res, &error);
-
-    if (error) {
-        g_printerr ("ERROR: %s\n", error->message);
-        g_error_free (error);
+void queueMessageCallback(SoupSession *session, SoupMessage *msg, gpointer user_data){
+    if(msg->status_code >= 200 && msg->status_code < 300){
+        g_print("Response: %s\n",msg->response_body->data);
     }
     else{
-        g_print("Success!");
-        g_input_stream_close(stream,NULL,&error);
+        g_printerr("ERROR: Code: %d\n",msg->status_code);
     }
+
+    GMainLoop *loop = (GMainLoop *)user_data;
+    g_main_loop_quit(loop);
 }
 
-void sendChannelMessage(SoupSession *session, std::string &text, std::string &skypeToken, std::string &channelId){
-    sendMessage(session,text,skypeToken,channelId);
+void sendChannelMessage(SoupSession *session, GMainLoop *loop, std::string &text, std::string &skypeToken, std::string &channelId){
+    sendMessage(session,loop,text,skypeToken,channelId);
 }
-
-//send message (destination determined by params string) and return async response object
-/* cpr::AsyncResponse sendMessage(std::string text, std::string& skypeToken, std::string& params){
-	cpr::AsyncResponse fr = cpr::PostAsync(
-		cpr::Url{ "https://amer.ng.msg.teams.microsoft.com/v1/users/ME/conversations/" + params + "/messages" },
-		cpr::Payload{ {"content",text},{"messagetype","Text"},{"contenttype","text"},{"asmreferences","[]"},{"properties","{importance:'',subject:null}"} },
-		cpr::Header{ {"Authentication","skypetoken=" + skypeToken} }
-	);
-
-	return fr;
-} */
