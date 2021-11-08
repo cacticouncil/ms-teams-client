f8b11df29856c73f18cc9939894c7681b7b4e85a
John Hook <jhook1@ufl.edu>
2021-11-08 16:47:08 -0500

    Added bash script, cpp sys func to trigger login

--- cplusplus/soup-client/include/polling.h
+++ cplusplus/soup-client/include/polling.h
@@ -1,6 +1,6 @@
 #pragma once
 
-void fetchPollingEndpoint(SoupSession *session, GMainLoop *loop, std::string &skypeToken);
+void initPolling(SoupSession *session, GMainLoop *loop, std::string &skypeToken);
 void pollEndpointCallback(SoupSession *session, SoupMessage *msg, gpointer user_data);
 void ArrayCallback(JsonArray* arr,guint index,JsonNode *elem,gpointer user_data);
 void poll(SoupSession *session, GMainLoop *loop, std::string &skypeToken, std::string &endpointUrl);
