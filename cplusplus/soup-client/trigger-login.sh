30f2e93200aac769537f482772f75a8269c1d4c5
John Hook <jhook1@ufl.edu>
2021-11-21 00:44:47 -0500

    Suppress output from vcxsrv trigger.

--- cplusplus/soup-client/trigger-login.sh
+++ cplusplus/soup-client/trigger-login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-powershell.exe "C:\'Program Files'\VcXsrv\vcxsrv.exe -multiwindow -clipboard -wgl -ac"
+powershell.exe "C:\'Program Files'\VcXsrv\vcxsrv.exe -multiwindow -clipboard -wgl -ac" >/dev/null 2>&1
 cd ../..
 export LIBGL_ALWAYS_INDIRECT=1
 npm run login
